ec6bd1254a07d2bd304e71bed99ed0b9fce9d619
Nicksan <inicksan1@gmail.com>
2020-08-24 17:56:52 +0300

    updated slider

--- js/my.js
+++ js/my.js
@@ -1,9 +1,23 @@
 let slider = $("#photos");
 
 slider.slick({
+  dots: false,
+  arrows: false,
   infinite: true,
-  speed: 500,
-  fade: true,
-  arrows: false
+  speed: 300,
+  slidesToShow: 2,
+  centerMode: true,
+  variableWidth: true
 });
+
+
+/*
+slider.slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  arrows: false,
+  autoplay: true
+});*/
 		
